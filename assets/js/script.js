80a93cc9a5aa50caac398c605c281c8c94cf119b
Josh Rehanek <joshrehanek@gmail.com>
2020-11-20 19:14:44 -0800

    adds arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// special characters
+const specialCharArray = ['@', '#', '$', '%', '<', '^', '<', '!'];
+// numbers
+const numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// lowerCase characters
+const lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// upperCase characters 
+const upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+// blank array to fill with the characters the user selects in the prompts
+let passwordCharacterArray = [];
+// blank string fo password to generate based on user preferences  
+let result = [];
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
